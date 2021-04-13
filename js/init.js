c1c0c6cc3b187cdc4fc5df09fd85f6d51ad763a0
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2021-04-12 23:09:15 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,19 @@
-// @ts-ignore
-var firebaseConfig = {
-  apiKey: "AIzaSyAbKxejM05gwN92VwJoK-hMFxOIgrxLP3g",
-  authDomain: "alondrahernandez-ef700.firebaseapp.com",
-  projectId: "alondrahernandez-ef700",
-  storageBucket: "alondrahernandez-ef700.appspot.com",
-  messagingSenderId: "1096214782339",
-  appId: "1:1096214782339:web:bfaa69b150ebee98e156b6"
-};
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.4.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAbKxejM05gwN92VwJoK-hMFxOIgrxLP3g",
+    authDomain: "alondrahernandez-ef700.firebaseapp.com",
+    projectId: "alondrahernandez-ef700",
+    storageBucket: "alondrahernandez-ef700.appspot.com",
+    messagingSenderId: "1096214782339",
+    appId: "1:1096214782339:web:bfaa69b150ebee98e156b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
