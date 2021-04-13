766926b76748d7bbe409ad494cd9e39bf5f80688
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2021-04-12 23:22:22 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,4 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.4.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js"></script>
 
 <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
@@ -12,7 +11,7 @@
     projectId: "alondrahernandez-ef700",
     storageBucket: "alondrahernandez-ef700.appspot.com",
     messagingSenderId: "1096214782339",
-    appId: "1:1096214782339:web:bfaa69b150ebee98e156b6"
+    appId: "1:1096214782339:web:aeee1e51e5dc644fe156b6"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
