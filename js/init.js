be310cd397cbd00864fb168ace56f0ed0152c006
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2021-04-12 23:53:22 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,13 @@
-
+<script>
+  // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyAbKxejM05gwN92VwJoK-hMFxOIgrxLP3g",
-    authDomain: "alondrahernandez-ef700.firebaseapp.com",
-    projectId: "alondrahernandez-ef700",
-    storageBucket: "alondrahernandez-ef700.appspot.com",
-    messagingSenderId: "1096214782339",
-    appId: "1:1096214782339:web:aeee1e51e5dc644fe156b6"
+    apiKey: "AIzaSyBRYAPTCrwSZHstmAlAcXAzrIlzJv2O0t8",
+    authDomain: "alondrahernandezx-62524.firebaseapp.com",
+    projectId: "alondrahernandezx-62524",
+    storageBucket: "alondrahernandezx-62524.appspot.com",
+    messagingSenderId: "528832472962",
+    appId: "1:528832472962:web:4a9cc4f4f642bd852a519b"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+</script>
