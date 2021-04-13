1e118595d5c90322a728727caf6366f18f2e0559
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2021-04-13 00:32:26 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,6 +3,7 @@
   var firebaseConfig = {
     apiKey: "AIzaSyBRYAPTCrwSZHstmAlAcXAzrIlzJv2O0t8",
     authDomain: "alondrahernandezx-62524.firebaseapp.com",
+    databaseURL: "https://alondrahernandezx.firebaseio.com", 
     projectId: "alondrahernandezx-62524",
     storageBucket: "alondrahernandezx-62524.appspot.com",
     messagingSenderId: "528832472962",
