94f33503402d6a57471aa9d64c277ff91c7c5d09
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2021-04-12 23:27:25 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,11 @@
-firebase.initializeApp({
+
+  var firebaseConfig = {
     apiKey: "AIzaSyAbKxejM05gwN92VwJoK-hMFxOIgrxLP3g",
     authDomain: "alondrahernandez-ef700.firebaseapp.com",
     projectId: "alondrahernandez-ef700",
     storageBucket: "alondrahernandez-ef700.appspot.com",
     messagingSenderId: "1096214782339",
     appId: "1:1096214782339:web:aeee1e51e5dc644fe156b6"
-  });
-  
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
