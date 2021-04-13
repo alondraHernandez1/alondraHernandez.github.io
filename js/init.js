e15f9eb4a4c1179e345eb48162ef02e30ad3a5ef
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2021-04-12 23:24:04 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,4 @@
-<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+firebaseConfig = {
     apiKey: "AIzaSyAbKxejM05gwN92VwJoK-hMFxOIgrxLP3g",
     authDomain: "alondrahernandez-ef700.firebaseapp.com",
     projectId: "alondrahernandez-ef700",
@@ -13,6 +6,4 @@
     messagingSenderId: "1096214782339",
     appId: "1:1096214782339:web:aeee1e51e5dc644fe156b6"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+  
